c339458cb10864eda063af1f21f5804e1b0e24c6
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-24 09:48:40 +0300

    add sort.h

---
+++ sort.h
@@ -0,0 +1,9 @@
+#ifndef SORT_H
+#define SORT_H
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif /* SORT_H */
