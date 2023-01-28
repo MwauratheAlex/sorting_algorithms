33c8a323e994423616df95f5a7d42845be59bb0e
MwauratheAlex <mwaurathealex@gmail.com>
2023-01-28 10:32:34 +0300

    add task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,24 +10,49 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current_node = (*list);
-	listint_t *moving_node = (*list);
+	listint_t *previous_node = (*list);
+	listint_t *current_node = previous_node->next;
 	listint_t *buffer = NULL;
 
 	while (current_node)
 	{
-		current_node = current_node->next;
-		moving_node = current_node->prev;
-		buffer = moving_node;
-		while (moving_node->prev && current_node->n < moving_node->n)
-			moving_node = moving_node->prev;
-		current_node->prev->next = current_node->next;
-		current_node->prev = moving_node->prev;
-		if (current_node->prev == NULL)
-			(*list) = current_node;
-		current_node->next = moving_node;
-		moving_node->prev = current_node;
-                current_node = buffer;
-		print_list(*list);
+		previous_node = current_node;
+		if (current_node->next)
+			current_node = previous_node->next;
+		else
+			break;
+
+		if (current_node->n < previous_node->n)
+		{
+			buffer = current_node;
+			previous_node->next = buffer->next;
+			if (buffer->next)
+				buffer->next->prev = previous_node;
+			current_node = previous_node;
+
+			while (previous_node && buffer)
+			{
+				previous_node = previous_node->prev;
+				if (previous_node && previous_node->n < buffer->n)
+				{
+					previous_node->next->prev = buffer;
+					buffer->next = previous_node->next;
+					buffer->prev = previous_node;
+					previous_node->next = buffer;
+					previous_node = current_node->prev;
+					buffer = NULL;
+					print_list(*list);
+				}
+				if (previous_node == NULL)
+				{
+					buffer->next = *list;
+					buffer->prev = NULL;
+					*list = buffer;
+					previous_node = current_node->prev;
+					buffer = NULL;
+					print_list(*list);
+				}
+			}
+		}
 	}
 }
--- 1-main.c
+++ 1-main.c
@@ -41,7 +41,7 @@ listint_t *create_listint(const int *array, size_t size)
 int main(void)
 {
     listint_t *list;
-    int array[] = {1, 2, 3, 0};
+    int array[] = {1,2 ,5, 4};
     size_t n = sizeof(array) / sizeof(array[0]);
 
     list = create_listint(array, n);
